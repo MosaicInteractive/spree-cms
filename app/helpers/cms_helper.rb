d17e14ac4027ca0f0d4c33582da2d8552dbe32bc
Maxime Robert-Schreyers <maxime.robert.s@gmail.com>
2010-10-19 10:48:51 +0200

    removed useless helpers

--- app/helpers/cms_helper.rb
+++ app/helpers/cms_helper.rb
@@ -1,16 +1,4 @@
 module CmsHelper 
-  # RAILS 3 TODO - no longer needed
-  def button_link_to_remote(text, url, html_options = {})
-    html_options.reverse_merge! :remote => true
-    link_to(text_for_button_link(text, html_options), url, html_options_for_button_link(html_options))
-  end
-
-  # def link_to_remote(name, options = {}, html_options = {})
-  #   options[:before] ||= "jQuery(this).parent().hide(); jQuery('#busy_indicator').show();"
-  #   options[:complete] ||= "jQuery('#busy_indicator').hide()"
-  #   link_to_function(name, remote_function(options), html_options || options.delete(:html))
-  # end
- 
   def linked_tag_list(tags)
     tags.collect {|tag| link_to(tag.name, tag_posts_url(:tag_name => tag.name ))}.join(", ")
   end
