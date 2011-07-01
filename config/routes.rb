4b013d16aa36443eb89cbe284fa57b3700f85660
Valentino Stoll <valentino@mosaicwebsite.com>
2011-07-01 08:47:25 -0400

    removed custom post routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :posts, :as => Spree::Config[:cms_permalink]
+  resources :posts#, :as => Spree::Config[:cms_permalink]
   match "#{Spree::Config[:cms_permalink]}/tags/:tag_name" => "posts#tags", :as => "tag_posts"
   resources :pages
   namespace :admin do
